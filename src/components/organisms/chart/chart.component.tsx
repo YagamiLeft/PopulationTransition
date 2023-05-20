c9ef8804f274b2d64f867ab32aa6e3f2788af60c
YagamiLeft <yagamileft3@gmail.com>
2023-05-20 13:00:25 +0900

    ReactEchartsの修正

--- src/components/organisms/chart/chart.component.tsx
+++ src/components/organisms/chart/chart.component.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactECharts, { EChartsOption } from 'echarts-for-react';
 
 export const ChartComponent: React.FC<{ chartOption: EChartsOption }> = ({ chartOption }) => {
-  return <ReactECharts option={chartOption} />;
+  return <ReactECharts option={chartOption} notMerge={true} lazyUpdate={true} />;
 };
